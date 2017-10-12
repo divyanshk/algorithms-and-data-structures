c3f951953b23f8f53ebfbf1c49cf33c0dfaaf06a
Divyansh Khanna <divyanshkhanna09@gmail.com>
2017-10-12 10:38:34 -0400

    leetcode

---
+++ HouseRobber.py
@@ -0,0 +1,18 @@
+# Problem: https://leetcode.com/problems/house-robber/description/
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[1], nums[0])
+        for i in xrange(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return dp[-1]
